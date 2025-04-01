d3fb4858c6f53388d6b9500ef051ac8cec0e6aa5
Qiumix <j487428862@gmail.com>
2025-04-01 21:04:10 +0800

    change c2_3

--- sicp/chapter2_3.py
+++ sicp/chapter2_3.py
@@ -38,23 +38,23 @@ def test_range_list():
 def odd():
     """"
     >>> odds = [1, 3, 5, 7]
-    >>> [x + 1 for i in odds]
+    >>> [x + 1 for x in odds]
     [2, 4, 6, 8]
-    >>> [x + 1 for i in odds if 25 % x]
+    >>> [x + 1 for x in odds if 25 % x == 0]
     [2, 6]
+    """
     # [<map expression> for <name> in <sequence expression> if <filter expression>]
-    """
 
 
-def devisor(n):
-    """
-    >>> divisors(4)
-    [1, 2]
-    >>> divisors(12)
-    [1, 2, 3, 4, 6]
-    >>> [for i in range(1, 1000) if sum(devisors(n)) == n]
-    [1, 6, 28, 464]
-    """
+def devisors(n):
+    # """
+    # >>> divisors(4)
+    # [1, 2]
+    # >>> divisors(12)
+    # [1, 2, 3, 4, 6]
+    # >>> [for i in range(1, 1000) if sum(devisors(n)) == n]
+    # [1, 6, 28, 464]
+    # """
     return [1] + [x for x in range(2, n) if n % x == 0]
 
 
@@ -67,10 +67,10 @@ def get_c(w, h):
     return w * 2 + h * 2
 
 
-def min_c(area, h):
+def min_c(area):
     """
     >>> min_c(80)
     36
     """
-    hs = devisor(area)
+    hs = devisors(area)
     return min([get_c(get_w(area, h), h) for h in hs])
